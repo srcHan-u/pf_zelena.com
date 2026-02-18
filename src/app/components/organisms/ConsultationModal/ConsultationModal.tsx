a4a43166559bfb2c71233bbb0ca9ee05205be158
Ferdinand Celine <boikoale97@gmail.com>
2026-02-18 10:20:37 +0200

    remove AbortedController

--- src/app/components/organisms/ConsultationModal/ConsultationModal.tsx
+++ src/app/components/organisms/ConsultationModal/ConsultationModal.tsx
@@ -129,7 +129,6 @@ export function ConsultationModal(
       const res = await fetch("/api/telegram", {
         method: "POST",
         body: formData,
-        signal: AbortSignal.timeout?.(20000),
       });
       if (!res.ok) throw new Error("Network response was not ok");
 
